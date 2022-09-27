40c4402a486430ff45003e9e81d15641dfac66b5
Donna R <112607918+donna2864@users.noreply.github.com>
2022-09-27 15:35:56 +0000

    problem 3 complete

---
+++ set01/problem03.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int sum_func(int a , int b);
+
+int main(void)
+{
+  int a, b, sum;
+  printf("Enter the first no.= ");
+  scanf("%d", &a);
+  printf("Enter the second no.= ");
+  scanf("%d", &b);
+  sum = sum_func(a,b);
+  printf("%d\n", sum);
+  return 0;
+}
+
+int sum_func(int a, int b)
+{ 
+  int sum;
+  sum = a+b;
+  return sum;
+}
